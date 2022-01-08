f1e5c95bace7558635ef51ddf5541261147572f2
letz0703 <letz0703@gmail.com>
2022-01-08 13:02:05 +0900

    finished design

--- app/src/main/java/com/letz/sqlite/MainActivity.java
+++ app/src/main/java/com/letz/sqlite/MainActivity.java
@@ -1,15 +1,36 @@
 package com.letz.sqlite;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView tvStart;
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvStart = findViewById(R.id.tvWelcom_Main);
+        btnStart = findViewById(R.id.btnStart_Main);
+
+        btnStart.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent igoQuizActivity = new Intent(MainActivity.this,QuizActivity.class);
+                startActivity(igoQuizActivity);
+                finish();
+            }
+        });
     }
+
 }
